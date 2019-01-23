1841ee852eb200071ca6b61d6ab5139a2424c5b1
Serj <1177133@mail.ru>
2019-01-23 12:03:01 +0600

    Lesson_1 HW4 - Calculator

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -6,31 +6,31 @@
 
 public class Calculator {
 
-    static int exponent(int a, int b) {
-        int result = 1;
-        for (int i = 1; i <= b; i++) {
-            result = result * a;
-        }
-        return result;
-    }
-
     public static void main(String[] args) {
         int num1 = 6;
         int num2 = 2;
-        char oper = '^';
+        char mathOperation = '^';
 
-        if (oper == '+') {
+        if (mathOperation == '+') {
             System.out.println(num1 + num2);
-        } else if (oper == '-') {
+        } else if (mathOperation == '-') {
             System.out.println(num1 - num2);
-        } else if (oper == '*') {
+        } else if (mathOperation == '*') {
             System.out.println(num1 * num2);
-        } else if (oper == '/') {
+        } else if (mathOperation == '/') {
             System.out.println(num1 / num2);
-        } else if (oper == '^') {
-            System.out.println(exponent(num1, num2));
-        } else if (oper == '%') {
+        } else if (mathOperation == '^') {
+            System.out.println(exponentCalculation(num1, num2));
+        } else if (mathOperation == '%') {
             System.out.println(num1 % num2);
         }
+    }
+
+    static int exponentCalculation(int a, int b) {
+        int result = 1;
+        for (int i = 1; i <= b; i++) {
+            result *= a;
+        }
+        return result;
     }
 }
