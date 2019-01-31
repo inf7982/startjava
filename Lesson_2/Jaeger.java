249722ac13da20e1cf4901f81f5b88fbf3ff7b87
Serj <1177133@mail.ru>
2019-01-31 09:47:27 +0600

    Lesson_2 HW4 - Jaeger

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -7,76 +7,12 @@
  */
 
 public class Jaeger {
-    private String name;
-    private int launched;
-    private String origin;
-    private double height;
-    private double weight;
-    private int speed;
-    private int strength;
-    private int armor;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getLaunched() {
-        return launched;
-    }
-
-    public void setLaunched(int launched) {
-        this.launched = launched;
-    }
-
-    public String getOrigin() {
-        return origin;
-    }
-
-    public void setOrigin(String origin) {
-        this.origin = origin;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
-    public int getStrength() {
-        return strength;
-    }
-
-    public void setStrength(int strength) {
-        this.strength = strength;
-    }
-
-    public int getArmor() {
-        return armor;
-    }
-
-    public void setArmor(int armor) {
-        this.armor = armor;
-    }
+    String name;
+    int launched;
+    String origin;
+    double height;
+    double weight;
+    int speed;
+    int strength;
+    int armor;
 }
--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -8,46 +8,44 @@
 
 public class JaegerTest {
     public static void main(String[] args) {
-        Jaeger robot1 = new Jaeger();
-        robot1.setName("Striker Eureka");
-        robot1.setLaunched(2019);
-        robot1.setOrigin("Australia");
-        robot1.setHeight(76.2);
-        robot1.setWeight(1.850);
-        robot1.setSpeed(10);
-        robot1.setStrength(10);
-        robot1.setArmor(9);
+        Jaeger strikerEureka = new Jaeger();
+        strikerEureka.name = "Striker Eureka";
+        strikerEureka.launched = 2019;
+        strikerEureka.origin = "Australia";
+        strikerEureka.height = 76.2;
+        strikerEureka.weight = 1.850;
+        strikerEureka.speed = 10;
+        strikerEureka.strength = 10;
+        strikerEureka.armor = 9;
 
-        System.out.println("Robot№1");
-        System.out.println("name: " + robot1.getName());
-        System.out.println("launched: " + robot1.getLaunched());
-        System.out.println("origin: " + robot1.getOrigin());
-        System.out.println("height: " + robot1.getHeight());
-        System.out.println("weight: " + robot1.getWeight());
-        System.out.println("speed: " + robot1.getSpeed());
-        System.out.println("strength: " + robot1.getStrength());
-        System.out.println("armor: " + robot1.getArmor());
+        System.out.println("name: " + strikerEureka.name);
+        System.out.println("launched: " + strikerEureka.launched);
+        System.out.println("origin: " + strikerEureka.origin);
+        System.out.println("height: " + strikerEureka.height);
+        System.out.println("weight: " + strikerEureka.weight);
+        System.out.println("speed: " + strikerEureka.speed);
+        System.out.println("strength: " + strikerEureka.strength);
+        System.out.println("armor: " + strikerEureka.armor);
 
         System.out.println();
 
-        Jaeger robot2 = new Jaeger();
-        robot2.setName("Cherno Alpha");
-        robot2.setLaunched(2015);
-        robot2.setOrigin("Russia");
-        robot2.setHeight(85.34);
-        robot2.setWeight(2.412);
-        robot2.setSpeed(3);
-        robot2.setStrength(10);
-        robot2.setArmor(10);
+        Jaeger chernoAlpha = new Jaeger();
+        chernoAlpha.name = "Cherno Alpha";
+        chernoAlpha.launched = 2015;
+        chernoAlpha.origin = "Russia";
+        chernoAlpha.height = 85.34;
+        chernoAlpha.weight = 2.412;
+        chernoAlpha.speed = 3;
+        chernoAlpha.strength = 10;
+        chernoAlpha.armor = 10;
 
-        System.out.println("Robot№2");
-        System.out.println("name: " + robot2.getName());
-        System.out.println("launched: " + robot2.getLaunched());
-        System.out.println("origin: " + robot2.getOrigin());
-        System.out.println("height: " + robot2.getHeight());
-        System.out.println("weight: " + robot2.getWeight());
-        System.out.println("speed: " + robot2.getSpeed());
-        System.out.println("strength: " + robot2.getStrength());
-        System.out.println("armor: " + robot2.getArmor());
+        System.out.println("name: " + chernoAlpha.name);
+        System.out.println("launched: " + chernoAlpha.launched);
+        System.out.println("origin: " + chernoAlpha.origin);
+        System.out.println("height: " + chernoAlpha.height);
+        System.out.println("weight: " + chernoAlpha.weight);
+        System.out.println("speed: " + chernoAlpha.speed);
+        System.out.println("strength: " + chernoAlpha.strength);
+        System.out.println("armor: " + chernoAlpha.armor);
     }
 }
