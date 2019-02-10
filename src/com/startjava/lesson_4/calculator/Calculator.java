8a766171ec602ca528348c2ce1cc9b56e1228c98
Serj <1177133@mail.ru>
2019-02-10 09:24:40 +0600

    Модифицировал программу Калькулятор

--- src/com/startjava/lesson_4/calculator/Calculator.java
+++ src/com/startjava/lesson_4/calculator/Calculator.java
@@ -3,13 +3,16 @@ package com.startjava.lesson_4.calculator;
 public class Calculator {
     private String mathematicalExpression;
 
+    public Calculator(String mathematicalExpression) {
+        this.mathematicalExpression = mathematicalExpression;
+    }
+
     public void calc() {
         String string = mathematicalExpression;
-        String[] parts = string.split("[+|-|*|/|^|%]");
-        String part1 = parts[0];
-        String part2 = parts[1];
-        int num1 = Integer.parseInt(part1);
-        int num2 = Integer.parseInt(part2);
+        string.replaceAll("\\s+","");
+        String[] numbers = string.split("[+|-|*|/|^|%]");
+        int num1 = Integer.parseInt(numbers[0]);
+        int num2 = Integer.parseInt(numbers[1]);
 
         if (string.contains("+")) {
             System.out.println(num1 + num2);
@@ -24,9 +27,5 @@ public class Calculator {
         } else if (string.contains("%")) {
             System.out.println((int) Math.IEEEremainder(num1, num2));
         }
-    }
-
-    public Calculator(String mathematicalExpression) {
-        this.mathematicalExpression = mathematicalExpression;
     }
 }
