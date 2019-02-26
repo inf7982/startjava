3efc18a09bf2c3642a4bd8d0168ee96a3f21fe8f
Serj <1177133@mail.ru>
2019-02-26 19:48:44 +0600

    Домашнее задание урока №5

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,2 +1,13 @@
 CREATE DATABASE jaegers;
-CREATE TABLE robots (id SERIAL PRIMARY KEY, modelName TEXT, mark CHAR(6), height FLOAT8, weight FLOAT8, status TEXT, origin TEXT, launch DATE, kaijuKill INTEGER);
+
+CREATE TABLE robots (
+	id SERIAL PRIMARY KEY
+	,modelName TEXT
+	,mark CHAR(6)
+	,height FLOAT8
+	,weight FLOAT8
+	,STATUS TEXT
+	,origin TEXT
+	,launch DATE
+	,kaijuKill INT
+	);
--- src/com/startjava/lesson_5/querise.sql
+++ src/com/startjava/lesson_5/querise.sql
@@ -1,27 +1,56 @@
 -- выведите всю таблицу
-SELECT * FROM robots;
+SELECT *
+FROM robots;
 
 -- отобразите только не уничтоженных роботов
-SELECT * FROM robots WHERE status != 'Destroyed';
+SELECT *
+FROM robots
+WHERE STATUS != 'Destroyed';
 
 -- отобразите роботов нескольких серий, например Mark-1 и Mark-6
-SELECT * FROM robots WHERE mark = 'Mark-1' OR mark = 'Mark-6';
+SELECT *
+FROM robots
+WHERE mark = 'Mark-1'
+	OR mark = 'Mark-6';
 
 -- отсортируйте таблицу по убыванию по столбцу mark
-SELECT * FROM robots ORDER BY mark DESC;
+SELECT *
+FROM robots
+ORDER BY mark DESC;
 
 -- отобразите самого старого робота
-SELECT * FROM robots WHERE launch = (SELECT MIN(launch) FROM robots);
+SELECT *
+FROM robots
+WHERE launch = (
+		SELECT MIN(launch)
+		FROM robots
+		);
 
 -- отобразите роботов, которые уничтожили больше/меньше всех kaiju
-SELECT * FROM robots WHERE kaijukill = (SELECT MAX(kaijukill) FROM robots);
-SELECT * FROM robots WHERE kaijukill = (SELECT MIN(kaijukill) FROM robots);
+SELECT *
+FROM robots
+WHERE kaijukill = (
+		SELECT MAX(kaijukill)
+		FROM robots
+		);
+
+SELECT *
+FROM robots
+WHERE kaijukill = (
+		SELECT MIN(kaijukill)
+		FROM robots
+		);
 
 -- отобразите средний вес роботов
-SELECT AVG(weight) FROM robots;
+SELECT AVG(weight)
+FROM robots;
 
 -- увеличьте на единицу количество уничтоженных kaiju у роботов, которые до сих пор не разрушены
-UPDATE robots SET kaijukill = kaijukill + 1 WHERE status != 'Destroyed';
+UPDATE robots
+SET kaijukill = kaijukill + 1
+WHERE STATUS != 'Destroyed';
 
 -- удалите уничтоженных роботов
-DELETE FROM robots WHERE status = 'Destroyed';
+DELETE
+FROM robots
+WHERE STATUS = 'Destroyed';
