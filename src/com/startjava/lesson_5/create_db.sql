11081820d8d89fc3f604abb60539b04530aa9890
Serj <1177133@mail.ru>
2019-02-26 11:43:58 +0600

    Домашнее задание урока №5

---
+++ src/com/startjava/lesson_5/create_db.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE jaegers;
+CREATE TABLE robots (id SERIAL PRIMARY KEY, modelName TEXT, mark CHAR(6), height FLOAT8, weight FLOAT8, status TEXT, origin TEXT, launch DATE, kaijuKill INTEGER);
---
+++ src/com/startjava/lesson_5/init_db.sql
@@ -0,0 +1,11 @@
+INSERT INTO robots(modelName, mark, height, weight, status, origin, launch, kaijuKill) VALUES
+('Gipsy Danger', 'Mark-3', '79.25', '1.981', 'Destroyed', 'United States of America', 'July 10, 2017', '9'),
+('Gipsy Avenger', 'Mark-6', '81.77', '2.004', 'Destroyed', 'Syria', 'January 12, 2034', '1'),
+('Cherno Alpha', 'Mark-1', '85.34', '2.412', 'Destroyed', 'Russia', 'June 25, 2015', '6'),
+('Coyote Tango', 'Mark-2', '85.34', '2.312', 'Active', 'Japan', 'December 30, 2015', '2'),
+('Saber Athena', 'Mark-7', '76.91', '1.628', 'Destroyed', 'Iran', 'March 16, 2018', '6'),
+('Striker Eureka', 'Mark-5', '76.21', '1.852', 'Destroyed', 'Australia', 'November 2, 2019', '11'),
+('Bracer Phoenix', 'Mark-5', '70.91', '2.128', 'Active', 'Iraq', 'November 1, 2025', '7'),
+('Brawler Yukon', 'Mark-1', '70.91', '2.128', 'Destroyed', 'United States of America', 'January 30, 2015', '2'),
+('Cherno Alpha', 'Mark-1', '85.34', '2.412', 'Destroyed', 'Russia', 'February 15, 2015', '6'),
+('Romeo Blue', 'Mark-1', '77.72', '7.775', 'Destroyed', 'United States of America', 'December 15, 2015', '2');
