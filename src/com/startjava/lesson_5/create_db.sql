2fbb69d82c5fabffb976de3a1be1998c3f78992d
Serj <1177133@mail.ru>
2019-02-27 11:16:53 +0600

    Домашнее задание урока №5

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,14 +1,13 @@
 CREATE DATABASE jaegers;
 
-CREATE TABLE robots
-(
-    id SERIAL PRIMARY KEY,
+CREATE TABLE robots (
+    id        SERIAL PRIMARY KEY,
     modelName TEXT,
-    mark CHAR(6),
-    height FLOAT8,
-    weight FLOAT8,
-    status TEXT,
-    origin TEXT,
-    launch DATE,
-    kaijuKill INT
+    mark      CHAR(6),
+    height    FLOAT8,
+    weight    FLOAT8,
+    status    TEXT,
+    origin    TEXT,
+    launch    DATE,
+    kaijuKill INTEGER
 );
