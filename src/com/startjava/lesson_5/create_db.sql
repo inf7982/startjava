677f220f36c16cde2063d75ee0b2c0af7c0736fb
Serj <1177133@mail.ru>
2019-02-28 19:48:55 +0600

    Обновление файла gitignore, добавил "*.iml"

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -2,13 +2,13 @@ CREATE DATABASE jaegers;
 
 CREATE TABLE robots
 (
-    id        SERIAL PRIMARY KEY,
-    modelName TEXT,
-    mark      CHAR(6),
-    height    FLOAT8,
-    weight    FLOAT8,
-    status    TEXT,
-    origin    TEXT,
-    launch    DATE,
-    kaijuKill INTEGER
+    id        SERIAL PRIMARY KEY NOT NULL,
+    modelName TEXT NOT NULL,
+    mark      CHAR(6) NOT NULL,
+    height    FLOAT8 NOT NULL,
+    weight    FLOAT8 NOT NULL,
+    status    TEXT NOT NULL,
+    origin    TEXT NOT NULL,
+    launch    DATE NOT NULL,
+    kaijuKill INTEGER NOT NULL
 );
